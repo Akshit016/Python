1e428cccbee680d5b8119ae25311a690a1845483
akshit016 <akshit.uriti@gmail.com>
2021-10-19 19:00:43 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# pizzas = ['cheese', 'chicken', 'paneer']
+# for _ in pizzas:
+#     print('This is a ', _.title(), ' Pizza,')
+#     print('\tI like', _.title(), ' Pizza.\n')
+
+# animals = ['dog', 'cat', 'pig', 'racoon']
+# for i in range(4):
+#     print(animals[i], 'is a great pet,')
+
+# print('\All animals are good pets.')
+
+# for value in range(5):
+#     print(value)
+
+numbers = list(range(6))
+print(numbers)
+
