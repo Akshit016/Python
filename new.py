2663a7abd9e15b92b567654d4e63bead79d1610d
akshit016 <akshit.uriti@gmail.com>
2021-10-20 22:17:03 +0530

    Chapter 5 - if statements

---
+++ new.py
@@ -0,0 +1,13 @@
+#chapter 5 - If Statements
+# cars = ['audi', 'bmw', 'subaru', 'toyata']
+
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
