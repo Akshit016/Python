a3bd1e049c9cf9ac7fcb009f97edf3b48ef08690
akshit016 <akshit.uriti@gmail.com>
2021-10-20 13:05:54 +0530

    adding main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+# for value in range(1,5):
+#     print(value)
+
+# numbers = list(range(6))
+# print(numbers)
+
+# even_numbers = list(range(2,13,2))
+# print(even_numbers)
+
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+#the above for loop is going to create a list of squares from 1 to 10
+print(squares)
