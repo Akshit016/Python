db65558511a704098e185ced46a4ad38064b77a4
akshit016 <akshit.uriti@gmail.com>
2021-10-20 20:20:56 +0530

    new commit

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-# pizzas = ['cheese', 'chicken', 'paneer']
-# for _ in pizzas:
-#     print('This is a ', _.title(), ' Pizza,')
-#     print('\tI like', _.title(), ' Pizza.\n')
+names = ['akshit', 'ashwith']
+print(names)
 
-# animals = ['dog', 'cat', 'pig', 'racoon']
-# for i in range(4):
-#     print(animals[i], 'is a great pet,')
-
-# print('\All animals are good pets.')
-
-# for value in range(5):
-#     print(value)
-
-numbers = list(range(6))
-print(numbers)
-
+names[0] = 'naresh'
+print(names)
